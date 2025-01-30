37e21e6effa52a43528d2928c510fab9a09750a1
Kaviya2205 <153165070+Kaviya2205@users.noreply.github.com>
2025-01-30 09:03:00 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+from scipy.spatial import distance
+
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    return (A + B) / (2.0 * C)
+
+def play_alarm(sound_file):
+    from playsound import playsound
+    playsound(sound_file)
